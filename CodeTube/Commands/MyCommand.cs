4455cb780a2205b47465c33247af7b8836050683
Himanshu kumar Sharma <SharmaHi@incadeadev.com>
2024-09-15 21:49:35 +0530

    Added Iframe Tool Window

---
+++ CodeTube/Commands/MyCommand.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading;
+using System;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace CodeTube
+{
+    [Command(PackageIds.MyCommand)]
+    internal sealed class MyCommand : BaseCommand<MyCommand>
+    {
+        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(CancellationToken.None);
+
+           // var package = await Package.GetGlobalServiceAsync(typeof(SVsServiceProvider)) as IServiceProvider;
+        }
+    }
+}
